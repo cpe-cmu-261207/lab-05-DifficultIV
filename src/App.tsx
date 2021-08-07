555ab297afcfe9de2f701381b0e94f552f965324
DifficultIV <kitpisan@gmail.com>
2021-08-07 10:05:10 +0700

    add footer header task todo componants

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,43 @@
-import React from 'react';
-import { useState } from 'react'
+
+
+import Footer from './Footer';
+
+import Header from './Header';
+import Todo from './Todo';
 
 function App() {
+//   const [title, setTitle] = useState('')
+//   const onKeyDownCallback = (ev: React.KeyboardEvent<HTMLInputElement>) => {
+//     /* check pressing enter key here */
+    
+//   }
+//   const addtask = () => { 
+//     console.log(title)
 
-  const onKeyDownCallback = (ev: React.KeyboardEvent<HTMLInputElement>) => {
-    /* check pressing enter key here */
-  }
+// }
 
   return (
     <div>
 
       {/* header section */}
-      <div className='flex justify-center items-end space-x-2'>
-        <span className='text-center italic my-2 text-2xl'>Minimal Todo List </span>
-        <span className='text-gray-400 italic my-2 text-xl'>by ...</span>
-      </div>
+      <Header></Header>
 
       {/* todo section */}
-      <div className='mx-auto max-w-4xl'>
+      <Todo></Todo>
+      {/* <div className='mx-auto max-w-4xl'> */}
 
         {/* task input and add button */}
-        <div className='flex space-x-1'>
-          <input className='border border-gray-400 w-full text-2xl'
+        {/* <div className='flex space-x-1'>
+          <input  onChange={event => setTitle(event.target.value)} className='border border-gray-400 w-full text-2xl'
             onKeyDown={onKeyDownCallback} ></input>
-          <button className='border border-gray-400 w-8 font-bold'>+</button>
-        </div>
+          <button className='border border-gray-400 w-8 font-bold' onClick={addtask} >+</button>
+        </div> */}
 
         {/* tasks section */}
-        <div>
+        {/* <div> */}
           {/* task example */}
           {/* Please convert this into a task component */}
-          <div
+          {/* <div
             className="flex justify-between h-8 items-center py-6 border-b"
           >
             <span className="text-2xl"> I am a task </span>
@@ -38,10 +45,10 @@ function App() {
               <button className="bg-green-400 w-24 text-2xl" >Done</button>
               <button className="bg-red-400 w-24 text-2xl" >Delete</button>
             </div>
-          </div>
+          </div> */}
 
           {/* another task example */}
-          <div
+          {/* <div
             className="flex justify-between h-8 items-center py-6 border-b"
           >
             <span className="text-2xl"> I am another task </span>
@@ -52,10 +59,10 @@ function App() {
           </div>
 
         </div>
-      </div>
+      </div> */}
 
       {/* footer section */}
-      <p className='text-center text-gray-400'> ... </p>
+      <Footer></Footer>
     </div>
   );
 }
